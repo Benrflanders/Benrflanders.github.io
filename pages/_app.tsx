c3edd871d375777bcc53ec477974c142180ea871
Ben Flanders <Benrflanders@gmail.com>
2023-03-04 17:49:06 -0500

    Hardcode GA4 ids

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,48 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import Script from "next/script";
 import * as gtag from "../lib/gtag";
-import {useEffect} from 'react';
-import TagManager from 'react-gtm-module';
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
-const GA_TRACKING_ID = gtag.GA_TRACKING_ID!;
+const GA_TRACKING_ID = "G-GLJBCSVJV2";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     try {
-      TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID!});
+      TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID! });
     } catch (error) {
-      console.error("Environment variable NEXT_PUBLIC_GTM_ID is not defined.")
+      console.error("Environment variable NEXT_PUBLIC_GTM_ID is not defined.");
     }
-    if(typeof(GA_TRACKING_ID) == "string"){
-      console.log("Google analytics ID: ", GA_TRACKING_ID )
-    }else{
-      throw TypeError("GA Tracking ID environment variable not loaded.")
+    if (typeof GA_TRACKING_ID == "string") {
+      console.log("Google analytics ID: ", GA_TRACKING_ID);
+    } else {
+      throw TypeError("GA Tracking ID environment variable not loaded.");
     }
-  }, [])
+  }, []);
 
-  return <>
-    <Script
-      id="load-gtag-js"
-      strategy='afterInteractive'
-      src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
-    <Script
-      id="push-gtag-to-datalayer"
-      strategy='afterInteractive'
-      dangerouslySetInnerHTML={{
-        __html: `
+  return (
+    <>
+      <Script
+        id="load-gtag-js"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="push-gtag-to-datalayer"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${gtag.GA_TRACKING_ID}', {
             page_path: window.location.pathname,
           });
-        `
-      }}
-    />
-    <Component {...pageProps} /></>
+        `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
